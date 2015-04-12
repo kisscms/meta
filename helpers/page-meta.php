746c2c20b4bc59db8773d42238068d3965df9af1
Makis Tracend <makis.tracend@gmail.com>
2015-04-12 05:55:48 -0700

    renaming fields view

--- helpers/page-meta.php
+++ helpers/page-meta.php
@@ -6,6 +6,8 @@ class PageMeta {
 	}
 
 	public static function fields( $data ){
+		$view = View::do_fetch('admin/page-meta-fields', $data );
+		return $view;
 	}
 
 	public static function save( $data ){
