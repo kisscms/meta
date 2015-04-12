bd2ff8369c2f728c62573127b84e2e844290d09e
Makis Tracend <makis.tracend@gmail.com>
2015-04-12 08:09:48 -0700

    :zap: data tweaks

--- helpers/page-meta.php
+++ helpers/page-meta.php
@@ -4,15 +4,17 @@ class PageMeta {
 
 	public static function edit( &$data ){
 		// add meta containers
-		foreach( $data['body'] as $body ){
+		foreach( $data['body'] as $k => $body ){
 			if( !array_key_exists('meta', $body ) ) $body['meta'] = array();
 			if( is_string( $body['meta'] ) ) $body['meta'] = json_decode($body['meta'], true);
+			// save reference
+			$data['body'][$k]['meta'] = $body['meta'];
 		}
 	}
 
 	public static function fields( &$data ){
-		$view = View::do_fetch('admin/page-meta-fields', $data );
-		return $view;
+		$view = View::do_fetch( getPath('views/admin/page-meta-fields.php'), $data );
+		echo $view; // return instead?
 	}
 
 	public static function save( &$data ){
