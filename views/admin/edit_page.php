8faa180e3f5ad987b25433cecf2ea5af9f7989a9
Makis Tracend <makis.tracend@gmail.com>
2015-04-12 03:09:57 -0700

    list meta as separate input fields

--- views/admin/edit_page.php
+++ views/admin/edit_page.php
@@ -1,6 +1,31 @@
 <?
 	if( !isset( $meta ) ) $meta = array();
 ?>
-<label for="meta">Meta</label>
-<!-- convert this to a rows UI element -->
-<textarea id="meta" name="meta"><?=$meta?></textarea>
+<h3>Meta</h3>
+<section id="meta">
+<?
+$index = 1;
+foreach( $meta as $key => $value ){
+?>
+<fieldset id="meta-<?=$index?>">
+	<input type="text" name="meta-key-<?=$index?>" value="<?=$key?>">
+	<input type="text" name="meta-value-<?=$index?>" value="<?=$value?>">
+</fieldset>
+<?
+$index++;
+}
+?>
+</section>
+<button id="meta-create">Add new</button>
+<script type="text/javascript">
+var admin_meta_data = <?=json_encode($meta) ?>;
+var admin_meta_data_length = admin_meta_data.length;
+$(document).ready(function(){
+	// events
+	$("#meta-create").click(function(){
+		admin_meta_data_length++;
+		var admin_meta_html = '<fieldset id="meta-'+ admin_meta_data_length +'"><input type="text" name="meta-key-'+ admin_meta_data_length +'" value=""><input type="text" name="meta-value-'+ admin_meta_data_length +'" value=""></fieldset>';
+		$("section#meta").append = admin_meta_html;
+	});
+});
+</script>
