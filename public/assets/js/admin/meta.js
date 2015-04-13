1a15316a42b151d5d1879c59934b4b00ec67258c
Makis Tracend <makis.tracend@gmail.com>
2015-04-12 17:54:25 -0700

    preventing default click event

--- public/assets/js/admin/meta.js
+++ public/assets/js/admin/meta.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	// events
-	$("#meta-create").click(function(){
+	$("#meta-create").click(function( e ){
+		e.preventDefault();
 		admin_meta_data_length++;
 		var admin_meta_html = '<fieldset id="meta-'+ admin_meta_data_length +'"><input type="text" name="meta-key-'+ admin_meta_data_length +'" value=""><input type="text" name="meta-value-'+ admin_meta_data_length +'" value=""></fieldset>';
 		$("section#meta").append( admin_meta_html );
