155026f2b49dd46b61b3b56b87c5819e4aa21b45
Makis Tracend <makis.tracend@gmail.com>
2015-04-12 17:59:36 -0700

    using colon(s) for event definition

--- bin/pages.php
+++ bin/pages.php
@@ -12,11 +12,11 @@ if( class_exists('Page') && method_exists('Page','register')){
 // add hooks
 if( class_exists('Controller') && method_exists('Controller','on') ){
 
-	Controller::on("admin-edit", 'PageMeta' );
+	Controller::on("admin:edit", 'PageMeta' );
 
-	Controller::on("admin-fields", 'PageMeta');
+	Controller::on("admin:fields", 'PageMeta');
 
-	Controller::on("admin-save", 'PageMeta' );
+	Controller::on("admin:save", 'PageMeta' );
 
 }
 
