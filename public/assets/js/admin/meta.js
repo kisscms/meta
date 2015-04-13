4c2c29ea786d09a0a66a21e8f13513da7f176615
Makis Tracend <makis.tracend@gmail.com>
2015-04-12 20:29:11 -0700

    :zap: delete actions

--- public/assets/js/admin/meta.js
+++ public/assets/js/admin/meta.js
@@ -3,14 +3,18 @@ $(document).ready(function(){
 	$("#meta-create").click(function( e ){
 		e.preventDefault();
 		admin_meta_data_length++;
-		var admin_meta_html = '<fieldset id="meta-'+ admin_meta_data_length +'"><a href="#" class="delete" align="right">[x]</a><input type="text" name="meta-key-'+ admin_meta_data_length +'" value=""><input type="text" name="meta-value-'+ admin_meta_data_length +'" value=""></fieldset>';
+		var admin_meta_html = '<fieldset id="meta-'+ admin_meta_data_length +'"><a class="delete" align="right">[x]</a><input type="text" name="meta-key-'+ admin_meta_data_length +'" value=""><input type="text" name="meta-value-'+ admin_meta_data_length +'" value=""></fieldset>';
 		$("section#meta").append( admin_meta_html );
+		metaDeleteActions();
 	});
+	metaDeleteActions();
+});
+
+function metaDeleteActions(){
 	$("section#meta fieldset a.delete").click(function( e ){
 		e.preventDefault();
 		$el = $(e.target).closest("fieldset");
 		$el.remove();
 		return false;
 	});
-});
-
+}
