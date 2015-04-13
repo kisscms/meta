c7ac0d49b0f171c21b3d6bcacd017a86682d2785
Makis Tracend <makis.tracend@gmail.com>
2015-04-12 20:19:56 -0700

    :zzz: stop link event

--- public/assets/js/admin/meta.js
+++ public/assets/js/admin/meta.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
 		var admin_meta_html = '<fieldset id="meta-'+ admin_meta_data_length +'"><a href="#" class="delete" align="right">[x]</a><input type="text" name="meta-key-'+ admin_meta_data_length +'" value=""><input type="text" name="meta-value-'+ admin_meta_data_length +'" value=""></fieldset>';
 		$("section#meta").append( admin_meta_html );
 	});
-	$("section#meta fieldset a.delete").click(function( e){
+	$("section#meta fieldset a.delete").click(function( e ){
+		e.preventDefault();
 		$el = $(e.target).closest("fieldset");
 		$el.remove();
 	});
